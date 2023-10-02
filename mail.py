6d5349a08f89633d8af1d21896b5e5bf4280da6a
Stanleyyek Kocanda <malyjozefek@gmail.com>
2023-10-02 17:05:04 +0200

    witajcie!

--- mail.py
+++ mail.py
@@ -4,19 +4,21 @@ niezrobione = []
 print("""MENU
 1 - Wcisnij 1 aby dodc zadanie!
 2 - Wcisnij aby zobaczyc swoje zadania!""")
-
 def  add_a_task():
-    zad = input("Tu wpisz zadanie jakie masz wykonac")
-    lista_zadan.append(zad) 
+  #  zad = input("Tu wpisz zadanie jakie masz wykonac")
+    zad = input("Tu wpisz zadanie jakie masz wykonac")  
+    slownik_czyzrobione = {'tresc_zadania' : zad, 'czyzrobione' : False}
+    lista_zadan.append(slownik_czyzrobione)
+
+def ifchecked():
     status = int(input("Jezeli wykonales zadanie wpisz 1. oznaczone zostanie jako zrobione, jesli natomiast zadania nie wykonales wpisz 0 oznaczymy je jako niezrobione")) 
     if status == 0:
         niezrobione.append(zad)
         print(niezrobione)
-        if status == 1:
-            zrobione.append(zad)
-            print(zrobione)
-        
-#add_a_task()
+    elif status == 1:
+        zrobione.append(zad)
+        print(zrobione)
+            
 
 def pokaz_zadan():
     for i in range(len(lista_zadan)):
@@ -29,10 +31,12 @@ def operowanie_menu():
         add_a_task()
     if operator == 2:
         pokaz_zadan()
-    add_a_task.status_zadania()
+    #ifchecked()  
+   
 
 while True:
     operowanie_menu()
+
 
 #praca domowa - poprawic wypisywanie zadan. Maja sie wyswietlac -  1, zadanie 2. zadanie 3. zadanie w nowych linijkach, ss na kanale generalnym dc
 
